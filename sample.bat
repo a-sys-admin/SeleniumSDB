8fa3b00d0071c66dfd10012e2bea197a37953d42
a-sys-admin <137617646+a-sys-admin@users.noreply.github.com>
2024-09-10 17:38:22 +0530

    Update sample.bat

--- sample.bat
+++ sample.bat
@@ -2,5 +2,9 @@
 REM Change directory to where the pom.xml is located
 cd /d C:\ProgramData\Jenkins\.jenkins\workspace\Selenium\NewBox-Add\NewBoxAddApproveTest
 
+REM Set the ChromeDriver path
+set CHROME_DRIVER_PATH=%CHROME_DRIVER_PATH%
+
 REM Run the Maven command with the specified test class
 mvn -f pom.xml -Dtest=dev.tests.CashPaymentTestclass test
+
